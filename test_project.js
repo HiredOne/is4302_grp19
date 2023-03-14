5d643da2673a9d6b679e6d39be45a27f8c444e38
JianXiang <changjianxiang99@gmail.com>
2023-03-14 12:45:04 +0800

    Update test_project.js

--- test_project.js
+++ test_project.js
@@ -20,53 +20,73 @@ contract('IS4302 Project', function (accounts) {
 
     console.log("Testing IS4302 Project");
 
+    // Assuming account[1] is admin
+ 
     it("Test Create New Role", async () => {
         // An account create new role request
-
+        // Account 2 create finance role
+        await requestApprovalManagementInstance.createNewRoleRequest("finance", { from: accounts[2] });
         // Admin account approve request
+        await requestApprovalManagementInstance.approveRequest(0, { from: accounts[1] });
+        // Check role contract if this role is created
+
     });
 
     it("Test Add Dataset to Role", async () => {
         // An account create Add Dataset to role request
-
+        await requestApprovalManagementInstance.addDatasetToRolesRequest("bank account", 0,  { from: accounts[2] });
         // Admin account approve request
+        await requestApprovalManagementInstance.approveRequest(1, { from: accounts[1] });
+        // Check Permissions contract if this dataset is created
 
     });
 
     it("Test Add Users to Roles", async () => {
        // An account create Add User to role request
-
+        await requestApprovalManagementInstance.addUsersToRolesRequest(1, 0,  { from: accounts[2] });
         // Admin account approve request
+        await requestApprovalManagementInstance.approveRequest(2, { from: accounts[1] });
+        // Check Roles contract if this user is added
 
     });
 
-
-    it("Test Remove Role", async () => {
-       // An account create Remove Role request
-
-        // Admin account approve request
-
-    });
-
-    it("Test Remove Dataset from Role", async () => {
-       // An account create Remove Dataset from Role request
-
-        // Admin account approve request
-
-    });
-
-    it("Test Remove User from Role", async () => {
-       // An account create Remove User from Role request
-
-        // Admin account approve request
-
-    });
 
     it("Test Reject Request", async () => {
         // An account create a request
+        await requestApprovalManagementInstance.createNewRoleRequest("business", { from: accounts[2] });
+        // Admin account reject request
+        await requestApprovalManagementInstance.rejectRequest(3, "Do not have permission to create this role", { from: accounts[1] });
+        await assert.equal("Do not have permission to create this role", await requestApprovalManagementInstance.getRequestAdminRemarks(6), "Request not rejected and admin remarks not updated");
+     });
+
+
+
+     // At the end of all other testings, we can test the remove 
+     it("Test Remove Dataset from Role", async () => {
+        // An account create Remove Dataset from Role request
+        await requestApprovalManagementInstance.removeDatasetFromRolesRequest(0, 0, { from: accounts[2] });
+        // Admin account approve request
+        await requestApprovalManagementInstance.approveRequest(4, { from: accounts[1] });
+        // Check Roles contract if this permissions is removed
+
+     });
  
-         // Admin account reject request
- 
+     it("Test Remove User from Role", async () => {
+        // An account create Remove User from Role request
+        await requestApprovalManagementInstance.removeUsersFromRolesRequest(1, 0, { from: accounts[2] });
+        // Admin account approve request
+        await requestApprovalManagementInstance.approveRequest(5, { from: accounts[1] });
+        // Check Roles contract if this user is removed
+
+     });
+
+     it("Test Remove Role", async () => {
+        // An account create Remove Role request
+        await requestApprovalManagementInstance.removeRoleRequest(0, 1, { from: accounts[2] });
+        // Admin account approve request
+        await requestApprovalManagementInstance.approveRequest(6, { from: accounts[1] });
+        // Check Roles contract if this role has been removed
+
      });
 
 })
