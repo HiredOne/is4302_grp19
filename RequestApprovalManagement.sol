e842128905a51f1884feb17d984ab642bdb0be80
JianXiang <changjianxiang99@gmail.com>
2023-03-23 15:26:50 +0800

    update

---
+++ RequestApprovalManagement.sol
@@ -0,0 +1,271 @@
+pragma solidity ^0.5.0;
+
+import "./User.sol";
+import "./Permission.sol";
+import "./Role.sol";
+import "./DatasetUploader.sol";
+
+// import "hardhat/console.sol";
+
+contract RequestApprovalManagement {
+    enum statusEnum {
+        pending,
+        approved,
+        rejected
+    }
+
+    enum requestTypeEnum {
+        createNewRoleRequest,
+        removeRoleRequest,
+        addExistingDatasetToRoleRequest,
+        removeDatasetFromRoleRequest,
+        addUserToRoleRequest,
+        removeUserFromRoleRequest,
+        uploadDatasetToNewRoleRequest,
+        uploadDatasetToExistingRoleRequest
+    }
+
+    struct Request {
+        address requesterAddress;
+        address adminAddress;
+        statusEnum requestStatus;
+        requestTypeEnum requestType;
+        uint256 creationDateTime;
+        uint256 updatedDateTime;
+        string adminAdditionalRemarks;
+        string roleName;
+        string datasetIdentifier;
+        uint256 roleID; 
+        uint256 permissionID;
+        address userID;
+    }
+
+    User userContract;
+    Permission permissionContract;
+    Role roleContract;   
+    DatasetUploader datasetuploaderContract;  
+
+    mapping(uint256 => Request) public requestsMapping;
+
+    uint256 totalNumberOfRequest;
+
+    event createNewRoleRequestApproved(string roleName);
+    event removeRoleRequestApproved(uint256 roleID, address userID);
+    event addExistingDatasetToRolesRequestApproved(uint256 permissionID, uint256 roleID);
+    event removeDatasetFromRolesRequestApproved(uint256 permissionID, uint256 roleID);
+    event addUsersToRolesRequestApproved(address userID, uint256 roleID);
+    event removeUsersFromRolesRequestApproved(address userID, uint256 roleID);
+    event uploadDatasetToNewRoleRequestApproved(string datasetIdentifier, string roleName, address requestor);
+    event uploadDatasetToExistingRoleRequestApproved(string datasetIdentifier, uint256 roleID, address requestor);
+
+    event rejectRequestEvent(uint256 requestID, string adminAdditionalRemarks);
+
+    constructor(User userAddress, Permission permissionAddress, Role roleAddress, DatasetUploader datasetUploaderAddress) public {
+        userContract = userAddress;
+        permissionContract = permissionAddress;
+        roleContract = roleAddress;
+        datasetuploaderContract = datasetUploaderAddress;
+        totalNumberOfRequest = 0;
+    }
+
+    // Admin functions
+    function approveRequest(uint256 requestID) public {
+        if (requestsMapping[requestID].requestType == requestTypeEnum.createNewRoleRequest) {
+
+            createNewRole(requestsMapping[requestID].roleName);
+
+        } else if (requestsMapping[requestID].requestType == requestTypeEnum.removeRoleRequest) {
+
+            removeRole(requestsMapping[requestID].roleID,requestsMapping[requestID].userID);
+
+        } else if (requestsMapping[requestID].requestType == requestTypeEnum.addExistingDatasetToRoleRequest) {
+
+            addExistingDatasetToRole(requestsMapping[requestID].permissionID, requestsMapping[requestID].roleID);
+
+        } else if (requestsMapping[requestID].requestType == requestTypeEnum.removeDatasetFromRoleRequest) {
+
+            removeDatasetFromRole(requestsMapping[requestID].permissionID, requestsMapping[requestID].roleID);
+
+        } else if (requestsMapping[requestID].requestType == requestTypeEnum.addUserToRoleRequest) {
+
+            addUsersToRole(requestsMapping[requestID].userID, requestsMapping[requestID].roleID);
+
+        } else if (requestsMapping[requestID].requestType == requestTypeEnum.removeUserFromRoleRequest) {
+
+            removeUsersFromRole(requestsMapping[requestID].userID, requestsMapping[requestID].roleID);
+
+        } else if (requestsMapping[requestID].requestType == requestTypeEnum.uploadDatasetToNewRoleRequest) {
+
+            uploadDatasetToNewRole(requestsMapping[requestID].datasetIdentifier, requestsMapping[requestID].roleName, requestsMapping[requestID].requesterAddress);
+
+        } else if (requestsMapping[requestID].requestType == requestTypeEnum.uploadDatasetToExistingRoleRequest) {
+
+            uploadDatasetToExistingRole(requestsMapping[requestID].datasetIdentifier, requestsMapping[requestID].roleID, requestsMapping[requestID].requesterAddress);
+
+        }       
+
+        requestsMapping[requestID].updatedDateTime = block.timestamp;
+        requestsMapping[requestID].adminAddress = msg.sender;
+        requestsMapping[requestID].requestStatus = statusEnum.approved;
+    }
+
+    function rejectRequest(uint256 requestID, string memory adminAdditionalRemarks) public {
+        requestsMapping[requestID].adminAdditionalRemarks = adminAdditionalRemarks;
+        requestsMapping[requestID].requestStatus = statusEnum.rejected;
+        emit rejectRequestEvent(requestID, adminAdditionalRemarks);
+    }
+
+    // Creating Requests
+
+    function createNewRoleRequest(string memory roleName) public {
+        Request memory newRequest = Request(msg.sender, address(0), statusEnum.pending, requestTypeEnum.createNewRoleRequest, block.timestamp,block.timestamp, "", roleName, "", 0,0,address(0));
+        requestsMapping[totalNumberOfRequest] = newRequest;
+        totalNumberOfRequest = totalNumberOfRequest + 1;
+    }
+
+    function removeRoleRequest(uint256 roleID, address userID) public {
+        Request memory newRequest = Request(msg.sender, address(0), statusEnum.pending, requestTypeEnum.removeRoleRequest, block.timestamp,block.timestamp, "", "", "", roleID,0,userID);
+        requestsMapping[totalNumberOfRequest] = newRequest;
+        totalNumberOfRequest = totalNumberOfRequest + 1;
+    }
+
+    function addExistingDatasetToRoleRequest(uint256 permissionID, uint256 roleID) public {
+        Request memory newRequest = Request(msg.sender, address(0), statusEnum.pending, requestTypeEnum.addExistingDatasetToRoleRequest, block.timestamp,block.timestamp, "", "", "", roleID,permissionID,address(0));
+        requestsMapping[totalNumberOfRequest] = newRequest;
+        totalNumberOfRequest = totalNumberOfRequest + 1;
+    }
+
+    function removeDatasetFromRoleRequest(uint256 permissionID, uint256 roleID) public {
+        Request memory newRequest = Request(msg.sender, address(0), statusEnum.pending, requestTypeEnum.removeDatasetFromRoleRequest, block.timestamp,block.timestamp, "", "", "", roleID, permissionID,address(0));
+        requestsMapping[totalNumberOfRequest] = newRequest;
+        totalNumberOfRequest = totalNumberOfRequest + 1;
+    }
+
+    function addUsersToRoleRequest(address userID, uint256 roleID) public {
+        Request memory newRequest = Request(msg.sender, address(0), statusEnum.pending, requestTypeEnum.addUserToRoleRequest, block.timestamp,block.timestamp, "", "", "", roleID, 0, userID);
+        requestsMapping[totalNumberOfRequest] = newRequest;
+        totalNumberOfRequest = totalNumberOfRequest + 1;
+    }
+
+    function removeUsersFromRoleRequest(address userID, uint256 roleID) public {
+        Request memory newRequest = Request(msg.sender, address(0), statusEnum.pending, requestTypeEnum.removeUserFromRoleRequest , block.timestamp,block.timestamp, "", "", "", roleID, 0, userID);
+        requestsMapping[totalNumberOfRequest] = newRequest;
+        totalNumberOfRequest = totalNumberOfRequest + 1;
+    }
+
+    function uploadDatasetToNewRoleRequest(string memory datasetIdentifier, string memory roleName) public {
+        Request memory newRequest = Request(msg.sender, address(0), statusEnum.pending, requestTypeEnum.uploadDatasetToNewRoleRequest , block.timestamp,block.timestamp, "", roleName, datasetIdentifier, 0, 0, address(0));
+        requestsMapping[totalNumberOfRequest] = newRequest;
+        totalNumberOfRequest = totalNumberOfRequest + 1;
+    }
+
+    function uploadDatasetToExistingRoleRequest(string memory datasetIdentifier, uint256 roleID) public {
+        Request memory newRequest = Request(msg.sender, address(0), statusEnum.pending, requestTypeEnum.uploadDatasetToExistingRoleRequest , block.timestamp,block.timestamp, "", "", datasetIdentifier, roleID, 0, address(0));
+        requestsMapping[totalNumberOfRequest] = newRequest;
+        totalNumberOfRequest = totalNumberOfRequest + 1;
+    }
+
+
+    // Getters
+
+    function getRequestStatus(uint256 requestID) public view returns(requestTypeEnum) {
+        return requestsMapping[requestID].requestType;
+    }
+
+    function getRequestAdminRemarks(uint256 requestID) public view returns(string memory) {
+        return requestsMapping[requestID].adminAdditionalRemarks;
+    }
+
+    function getTotalNumberOfRequests() public view returns(uint256) {
+        return totalNumberOfRequest;
+    }
+
+
+
+    // Helper functions
+    
+    function createNewRole(string memory roleName) public {
+        // Role Creation
+        roleContract.createRole(roleName);
+
+        emit createNewRoleRequestApproved(roleName);
+    }
+
+    function removeRole(uint256 roleID, address userID) public {
+        //  Remove role
+        roleContract.removeRoleUser(roleID, userID);
+
+        emit removeRoleRequestApproved(roleID, userID);
+    }
+
+    function addExistingDatasetToRole(uint256 permissionID, uint256 roleID) public {
+        // Give Permission to role
+        permissionContract.givePermissionRole(permissionID, roleID);
+        // Give role to permission
+        roleContract.giveRolePermission(roleID, permissionID); 
+
+        emit addExistingDatasetToRolesRequestApproved(permissionID, roleID);
+    }
+
+    function removeDatasetFromRole(uint256 permissionID, uint256 roleID) public {
+        // Remove Permission to role
+        permissionContract.removePermissionRole(permissionID, roleID); 
+        // Remove role to permission
+        roleContract.removeRolePermission(roleID, permissionID); 
+        
+        emit removeDatasetFromRolesRequestApproved(permissionID, roleID);
+    }
+
+    function addUsersToRole(address userID, uint256 roleID) public {
+        // Give user to role
+        roleContract.giveRoleUser(roleID, userID);
+        // Give role to user
+        userContract.giveUserRole(userID, roleID);
+
+        emit addUsersToRolesRequestApproved(userID, roleID);
+    }
+
+    function removeUsersFromRole(address userID, uint256 roleID) public {
+        // Remove user from role
+        roleContract.removeRoleUser(roleID, userID);
+        // Remove role from user
+        userContract.removeUserRole(userID, roleID);
+
+        emit removeUsersFromRolesRequestApproved(userID, roleID);
+    }
+
+    function uploadDatasetToNewRole(string memory datasetIdentifier, string memory roleName, address requestor) public {
+        // Create Permission
+        uint256 permissionID = permissionContract.createPermission(datasetIdentifier);
+        
+        // Role Creation
+        uint256 roleID = roleContract.createRole(roleName);
+
+        // Upload dataset
+        datasetuploaderContract.uploadDataset(datasetIdentifier,requestor);
+
+        // Give Permission to role
+        permissionContract.givePermissionRole(permissionID, roleID);
+        // Give role to permission
+        roleContract.giveRolePermission(roleID, permissionID); 
+
+        emit uploadDatasetToNewRoleRequestApproved(datasetIdentifier, roleName);
+    }
+
+    function uploadDatasetToExistingRole(string memory datasetIdentifier, uint256 roleID, address requestor) public {
+        // Create Permission
+        uint256 permissionID = permissionContract.createPermission(datasetIdentifier);
+
+        // Upload dataset
+        datasetuploaderContract.uploadDataset(datasetIdentifier,requestor);
+
+        // Give Permission to role
+        permissionContract.givePermissionRole(permissionID, roleID);
+        // Give role to permission
+        roleContract.giveRolePermission(roleID, permissionID); 
+
+       emit uploadDatasetToExistingRoleRequestApproved(datasetIdentifier, roleID);
+    }
+
+
+}
---
+++ test_js.js
@@ -0,0 +1,83 @@
+const _deploy_contracts = require("../migrations/2_deploy_contracts");
+const truffleAssert = require("truffle-assertions"); //npm i truffle-assertions
+const BigNumber = require('bignumber.js'); // npm install bignumber.js
+var assert = require("assert");
+
+var User = artifacts.require("../contracts/User.sol");
+var Permission = artifacts.require("../contracts/Permission.sol");
+var Role = artifacts.require("../contracts/Role.sol");
+var RequestApprovalManagement = artifacts.require("../contracts/RequestApprovalManagement.sol");
+
+
+
+contract('IS4302 Project', function (accounts) {
+    before(async () => {
+        userInstance = await User.deployed();
+        permissionInstance = await Permission.deployed();
+        roleInstance = await Role.deployed();
+        requestApprovalManagementInstance = await RequestApprovalManagement.deployed();
+    });
+
+    console.log("Testing IS4302 Project");
+
+    it("Acc1 upload a dataset to a new role", async () => {
+        await assert.equal(0, await requestApprovalManagementInstance.getTotalNumberOfRequests());
+        await requestApprovalManagementInstance.uploadDatasetToNewRoleRequest("schema1.table1.column1", "role0" ,{ from: accounts[2] });
+        await assert.equal(1, await requestApprovalManagementInstance.getTotalNumberOfRequests());
+        // Admin account approve request
+        let requestApproved = await requestApprovalManagementInstance.approveRequest(0, { from: accounts[1] });
+
+        // Check if new role is created and new permissions is created
+
+        truffleAssert.eventEmitted(requestApproved, "uploadDatasetToNewRoleRequestApproved");
+    });
+
+
+ 
+    it("Create a new role1", async () => {
+        // An account create new role request
+        // Account 2 create role1
+        await assert.equal(1, await requestApprovalManagementInstance.getTotalNumberOfRequests());
+        await requestApprovalManagementInstance.createNewRoleRequest("role1", { from: accounts[2] });
+        await assert.equal(2, await requestApprovalManagementInstance.getTotalNumberOfRequests());
+        // Admin account approve request
+        let requestApproved = await requestApprovalManagementInstance.approveRequest(1, { from: accounts[1] });
+        
+        // Check role contract if this role is created
+
+        
+        truffleAssert.eventEmitted(requestApproved, "createNewRoleRequestApproved");
+
+    });
+
+    it("Add permission0 to role1", async () => {
+        // acc2 create Add per,ission0 to role1 request
+        await assert.equal(2, await requestApprovalManagementInstance.getTotalNumberOfRequests());
+        await requestApprovalManagementInstance.addExistingDatasetToRoleRequest(0, 1,  { from: accounts[2] });
+        await assert.equal(3, await requestApprovalManagementInstance.getTotalNumberOfRequests());
+        // Admin account approve request
+        let requestApproved = await requestApprovalManagementInstance.approveRequest(2, { from: accounts[1] });
+        
+        // Check Permission contract if this dataset is created
+
+        truffleAssert.eventEmitted(requestApproved, "addExistingDatasetToRolesRequestApproved");
+        
+    });
+
+    it("Add acc2 to role1", async () => {
+       // acc2 create request to add acc2 to role1
+        await assert.equal(3, await requestApprovalManagementInstance.getTotalNumberOfRequests());
+        await requestApprovalManagementInstance.addUsersToRoleRequest(accounts[2], 1,  { from: accounts[2] });
+        await assert.equal(4, await requestApprovalManagementInstance.getTotalNumberOfRequests());
+        // Admin account approve request
+        let requestApproved = await requestApprovalManagementInstance.approveRequest(3, { from: accounts[1] });
+        
+        // Check Role contract if this user is added
+
+        truffleAssert.eventEmitted(requestApproved, "addUsersToRolesRequestApproved");
+    });
+
+
+   
+
+})
