4733f84ad1e2ca775b104a6d0d51f9a91be78834
JesterTanMZ <77263734+JesterTanMZ@users.noreply.github.com>
2023-03-27 19:51:42 +0800

    Update DatasetUploader

--- DatasetUploader.sol
+++ DatasetUploader.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.5.0;
 import "./User.sol";
 import "./Permission.sol";
 import "./Role.sol";
-import "./Metadata.sol";
-import "./Pointer.sol";
 import "./DataLineage.sol";
 
 
@@ -14,39 +12,40 @@ contract DatasetUploader {
     User userContract;
     Permission permissionContract;
     Role roleContract;   
-    Metadata metadataContract;
-    Pointer pointerContract;
     DataLineage dataLineageContract;
     
     address public contractDeployer;
     uint256 private rewardAmount = 1;
 
-    event datasetUploaded(string datasetIdentifier, uint256 roleID, address requestor);
+    event datasetUploaded(string datasetIdentifier, address requestor);
 
-    constructor(address dataLineageAddress) public {
-        dataLineage = DataLineage(dataLineageAddress);
+    constructor(User userAddress, Permission permissionAddress, Role roleAddress, DataLineage dataLineageAddress) public {
+        userContract = userAddress;
+        permissionContract = permissionAddress;
+        roleContract = roleAddress;
+        dataLineageContract = dataLineageAddress;
         contractDeployer =  msg.sender;
     }
 
     //This function is called by the user to upload a dataset, a new permission will be created and tagged to the new role created
     function uploadDatasetToNewRole(string memory datasetIdentifier, string memory roleName, string memory pointer, address requestor, string memory permissionName) public {
-        permissionContract.addPermission(permissionName);
+        permissionContract.createPermission(permissionName);
         roleContract.createRole(roleName); //Can this return the roleID? 
-        roleContract.assignPermission(roleContract.numRoles, permissionName); //Is permission identified by name or ID? 
-        dataLineageContract.addDataset(datasetIdentifier, roleContract.numRoles, pointer, requestor);
+        roleContract.assignPermission(1, permissionContract.getNumPermissions()); //Is permission identified by name or ID? 
+        dataLineageContract.addNewDataset(datasetIdentifier, pointer, requestor);
 
         userContract.giveTokens(requestor, rewardAmount); //Where are the tokens coming from? Should it not be a transfer?
-        emit datasetUploaded(datasetIdentifier, roleContract.numRoles, requestor);
+        emit datasetUploaded(datasetIdentifier, requestor);
     }
 
     //This function is called by the user to upload a dataset, a permission will be created and tagged to the an exisitng role
     function uploadDatasetToExistingRole(string memory datasetIdentifier, uint256 roleID, string memory pointer, address requestor, string memory permissionName) public {
-        permissionContract.addPermission(permissionName);
-        roleContract.assignPermission(roleID, permissionName); //Is permission identified by name or ID? -JESTER
-        dataLineageContract.addDataset(datasetIdentifier, roleID, pointer, requestor);
+        permissionContract.createPermission(permissionName);
+        roleContract.assignPermission(roleID, permissionContract.getNumPermissions()); //Is permission identified by name or ID? -JESTER
+        dataLineageContract.addNewDataset(datasetIdentifier, pointer, requestor);
         
         userContract.giveTokens(requestor, rewardAmount);
-        emit datasetUploaded(datasetIdentifier, roleContract.numRoles, requestor);
+        emit datasetUploaded(datasetIdentifier, requestor);
     }
 
 }
